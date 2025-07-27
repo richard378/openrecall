e51046b968c85b46380d2a8e074a62007d99e658
Richard Horan <richard@webhoran.com>
2025-07-27 12:34:43 -0400

    working on slider error

--- src/openrecall/app.py
+++ src/openrecall/app.py
@@ -101,7 +101,7 @@ def timeline():
       <div class="slider-value" id="sliderValue">{{timestamps[0] | timestamp_to_human_readable }}</div>
     </div>
     <div class="image-container">
-      <img id="timestampImage" src="/static/{{filenames[slider.value-1]}}" alt="Image for timestamp">
+      <img id="timestampImage" src="/static/{{filenames|length - 1}}" alt="Image for timestamp">
     </div>
   </div>
   <script>
